85bb3dedd55a07e404fd708a8c86e63841158df0
Paul Cooper <paul@ah-studios.com>
2021-06-02 00:54:49 +0100

    fix for french localisation

--- lang/fr-FR.js
+++ lang/fr-FR.js
@@ -1,7 +1,7 @@
 export default {
     // Home
     HOME_SUMMARY: 'Développeur Front End basé sur {emplacement}, photographe amateur et nerd technique avoué travaillant chez {entreprise}',
-    HOME_BODY: 'En un mot, je suis un développeur front-end ambitieux et passionné. Je m\'efforce de produire de beaux produits qui inspirent et ravissent. J'ai un peu plus d'une décennie d'expérience dans l'industrie à la fois en conception et en développement. J\'ai travaillé avec des gens formidables, dans des entreprises innovantes pour certaines marques leaders du secteur.',
+    HOME_BODY: 'En un mot, je suis un développeur front-end ambitieux et passionné. Je m\'efforce de produire de beaux produits qui inspirent et ravissent. J\'ai un peu plus d\'une décennie d\'expérience dans l\'industrie à la fois en conception et en développement. J\'ai travaillé avec des gens formidables, dans des entreprises innovantes pour certaines marques leaders du secteur.',
     LOCATION_SHORT: 'Londres',
     LOCATION: 'Londres, Royaume-Uni',
 };
