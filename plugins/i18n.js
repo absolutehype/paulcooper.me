b393f3c23ba579cbdee60791dadaed750406c767
Paul Cooper <paul@ah-studios.com>
2025-02-13 19:06:45 +0000

    nextjs application build

--- plugins/i18n.js
+++
@@ -1,6 +0,0 @@
-export default function ({ app, store }) {
-    // on change locale
-    app.i18n.beforeLanguageSwitch = (oldLocale, newLocale) => {
-        app.i18n.setLocaleCookie(newLocale);
-    };
-}
